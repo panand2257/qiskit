da7122d43995e181ef881995f0ac39480381be23
panand2257 <panand2257@gmail.com>
2021-05-26 16:25:33 +0200

    qsk1

--- Chapter_1/Atoms_and_Computation/ex1.py
+++ Chapter_1/Atoms_and_Computation/ex1.py
@@ -1,2 +1,15 @@
 from qiskit import QuantumCircuit, assemble, Aer
 from qiskit.visualization import plot_histogram
+from qiskit_textbook.widgets import binary_widget
+
+binary_widget(n=5)
+n = 8
+n_c = n
+n_p = n
+
+qc = QuantumCircuit(n_c, n_p)
+
+for j in range(n):
+    qc.measure(j,j)
+
+qc.draw()
